e00d47a6ba68717eea3a846a0e1a174777fad14e
ABHINESH <22n201@psgtech.ac.in>
2024-04-20 15:06:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Page1 from './Pages/Page1.js'
+import Loginstaff from './Pages/Loginstaff.js';
+import Loginstudent from './Pages/Loginstudent.js';
+import Attendancepage from './Pages/Attendancepage.js';
+import Staffattendance from './Pages/Staffattendance.js';
+
+
+function App() {
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default App;
